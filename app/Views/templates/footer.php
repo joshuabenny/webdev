ec3cc7eefd82020364865f05a0bedba452581727
joshuabenny <100258658+joshuabenny@users.noreply.github.com>
2024-04-16 19:13:53 +0100

    Updated Footer
    Footer updated with relevant information

--- app/Views/templates/footer.php
+++ app/Views/templates/footer.php
@@ -1,3 +1,9 @@
-<em>&copy; 2024</em>
+<div style="position: relative">
+            <p style="position: fixed; bottom: 0; width:100%; text-align: center">
+            <i> &copy; 2024 tracking api from tracker.gg<br>
+            live data depends on the api and the game developers
+    </i>
+            </p>
+        </div>
 </body>
 </html>
