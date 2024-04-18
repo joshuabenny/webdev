c6268122e816c7752c7b632b94923496814800b9
joshuabenny <100258658+joshuabenny@users.noreply.github.com>
2024-04-18 22:34:21 +0100

    Bootstrap
    Changed style and added bootstrap

--- app/Views/pages/about.php
+++ app/Views/pages/about.php
@@ -1,4 +1,7 @@
 <!doctype html>
 
-<h1> Hello this is the about's page </h1>
-<h2>second line</h2>
+<div style="position:fixed; width:100%; text-align: center" >
+            <p style="font-size: 2.1vw;">
+            If you need to get all of your information for Apex, Valorant or Rainbow Six
+            </p>
+        </div>
--- app/Views/templates/header.php
+++ app/Views/templates/header.php
@@ -2,22 +2,36 @@
 <html>
 <head>
     <title>Game Stats</title>
-    <link rel="stylesheet" href="https://bootswatch.com/3/darkly/bootstrap.min.css">
+    <link rel="stylesheet" href="https://bootswatch.com/5/quartz/bootstrap.min.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="refresh" content="3600">
 </head>
 <body>
-    <nav class="navbar navbar-inverse">
-      <div class="container">
-        <div class="navbar-header">
-          <a class="navbar-brand" href="<?php echo base_url(); ?>">Game Stats</a>
-        </div>
-        <div id="navbar">
-          <ul class="nav navbar-nav">
-            <li><a href="<?=base_url()?>home">Home</a></li>
-            <li><a href="<?=base_url()?>about">About</a></li>
-            <li><a href="<?=base_url()?>news">News</a></li>
-            <li><a href="<?=base_url()?>news/new">Create News</a></li>
+    <nav class="navbar navbar-expand-lg bg-dark" data-bs-theme="dark">
+  <div class="container-fluid">
 
-          </ul>
-          <br>
-    </nav>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarColor02" aria-controls="navbarColor02" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarColor02">
+      <ul class="navbar-nav me-auto">
+        <li class="nav-item">
+          <a class="nav-link active" href="<?=base_url()?>home">Home
+            <span class="visually-hidden">(current)</span>
+          </a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="<?=base_url()?>news">News</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="<?=base_url()?>news/new">Create News</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="<?=base_url()?>about">About</a>
+        </li>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
     <h1><?= esc($title) ?></h1>
